d838ee4eea352c29ccd735d06b45131d912919e7
Aditya Kumar <84345234+AadityaKumaar@users.noreply.github.com>
2025-01-07 05:43:43 +0530

    updated test redirect link

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from 'react';
 export default function LoginPage(): React.JSX.Element {
 
   useEffect(() => {
-    window.location.replace("https://bace.org.in/prajna2025")
+    window.location.replace("https://bace.org.in/test/prajna2025")
   }, [])
 
   return (
